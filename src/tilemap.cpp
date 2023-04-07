568aaa9dd4a5a0e7e5f66e48e282181d18a8801c
Grayson <gclark1013@gmail.com>
2023-04-07 13:54:37 -0400

    fix block movement

--- src/tilemap.cpp
+++ src/tilemap.cpp
@@ -6,10 +6,6 @@
 #include <chrono>
 #include "input.h"
 
-//TileProperties tileset[2] = {
-    //{true, getSprites()[TetrisBlocks::Wall]},
-	//{false, getSprites()[TetrisBlocks::BG]}//want to access stored sprite in sprite rrenderer
-//};
 
 char tilemap[22][12] =//dont know how I will draw this
 {
@@ -58,8 +54,8 @@ Block activeBlock;
 std::vector<Block> inactiveBlocks;
 
 void ClearGridExceptWalls() {
-	for (int row = 1; row < 22 - 2; row++) {
-		for (int col = 1; col < 12 - 2; col++) {
+	for (int row = 1; row < 22 - 1; row++) {
+		for (int col = 1; col < 12 - 1; col++) {
 			tilemap[row][col] = 0;
 		}
 	}
@@ -71,7 +67,6 @@ void UpdateGridWithBlock(Block block) {
 		Vector2Int fillSquare = pieceCenter + offset;
 		tilemap[fillSquare.y][fillSquare.x] = block.type;
 	}
-
 }
 void UpdateGrid() {
 	ClearGridExceptWalls();
@@ -79,7 +74,6 @@ void UpdateGrid() {
 		UpdateGridWithBlock(b);
 	}
 	UpdateGridWithBlock(activeBlock);
-
 }
 
 int RandomRange(int min, int max) {
@@ -103,34 +97,54 @@ void InitTilemap() {
 
 
 
-bool isMoveValid() {
+bool isMoveValid(Vector2Int movementOffset) {
 	std::vector<Vector2Int> offsets = Cells[activeBlock.type];
 	Vector2Int tileCenterPos = activeBlock.pos;
-		for (Vector2Int offset : offsets) {
-			Vector2Int tileToCheck = tileCenterPos + offset;
-			tileToCheck.y += 1;
-			bool isOwnBlock = false;
-			for (Vector2Int offsetAgain : offsets) {
-				if (tileToCheck == offsetAgain + tileCenterPos) isOwnBlock = true;
-			}
-			if (isOwnBlock) continue;
-			bool isCollision = tilemap[tileToCheck.y][tileToCheck.x] != 0;
-			if (isCollision) {
-				return false;
-			}
+	for (Vector2Int offset : offsets) {
+		Vector2Int tileToCheck = tileCenterPos + offset;
+		tileToCheck = tileToCheck + movementOffset;
+
+		// make sure we don't collide with ourself
+		bool isOwnBlock = false;
+		for (Vector2Int offsetAgain : offsets) {
+			if (tileToCheck == offsetAgain + tileCenterPos) isOwnBlock = true;
 		}
-		return true;
+		if (isOwnBlock) continue;
 
-
-}
-void step() {
-	if (!isMoveValid()) {
-		inactiveBlocks.push_back(activeBlock);
-		activeBlock = CreatRandomBlockAtStartPos();
+		char squareWeAreCollidingWith = tilemap[tileToCheck.y][tileToCheck.x];
+		bool isCollision = squareWeAreCollidingWith != 0;
+		if (isCollision) {
+			return false;
+		}
 	}
-	else {
+	return true;
+}
 
-		activeBlock.pos.y += 1;
+void LockActiveBlock() {
+	inactiveBlocks.push_back(activeBlock);
+	activeBlock = CreatRandomBlockAtStartPos();
+}
+
+void TryMoveActiveBlock(InputAction direction) {
+	if (direction == InputAction::DOWN) {
+		// if we're trying to move down and can't - we lock the block and spawn a new one
+		if (!isMoveValid(Vector2Int(0, 1))) {
+			LockActiveBlock();
+		}
+		else {
+			activeBlock.pos.y += 1;
+		}
+	}
+	// for moving left/right, if we try to but can't, don't spawn a new one. Just do nothing
+	else if (direction == InputAction::LEFT) {
+		if (isMoveValid(Vector2Int(-1, 0))) {
+			activeBlock.pos.x -= 1;
+		}
+	}
+	else if (direction == InputAction::RIGHT) {
+		if (isMoveValid(Vector2Int(1, 0))) {
+			activeBlock.pos.x += 1;
+		}
 	}
 }
 
@@ -139,17 +153,19 @@ void Tiletime() {
 	long long seconds = std::chrono::duration_cast<std::chrono::seconds>(std::chrono::system_clock::now().time_since_epoch()).count();
 	long long secondPassedSinceLastFrame = seconds - previousTime;
 	if (secondPassedSinceLastFrame >= 1) {
-		step();
+		TryMoveActiveBlock(InputAction::DOWN);
 	}
 	previousTime = seconds;
+
+
 	if (isActionPressed(InputAction::LEFT)) {
-		activeBlock.pos.x -= 1;
+		TryMoveActiveBlock(InputAction::LEFT);
 	}
 	else if (isActionPressed(InputAction::RIGHT)) {
-		activeBlock.pos.x += 1;
+		TryMoveActiveBlock(InputAction::RIGHT);
 	}
 	else if (isActionPressed(InputAction::DOWN)) {
-		step();
+		TryMoveActiveBlock(InputAction::DOWN);
 	}
 }
 
